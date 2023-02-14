70ed18d194184d9b712a002a9a697fa0838f93fd
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-15 01:50:37 +0530

    makemigrations and migrate

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Student
 # Register your models here.
+@admin.register(Student)
+class AdminStudent(admin.ModelAdmin):
+    list_display=['id','name','roll','course']
