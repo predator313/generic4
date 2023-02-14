8cc2d4de2b8ecabcc470debcf3cdd048e135da7f
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-15 02:04:03 +0530

    making listview

--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from .models import Student
 # Create your views here.
+class StudentListview(ListView):
+    model=Student
