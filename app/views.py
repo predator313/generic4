5a3549e70ae909c6adf472ad0f0c2ef2b52690fd
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-15 02:37:52 +0530

    templates_name_suffix and ordering

--- app/views.py
+++ app/views.py
@@ -4,3 +4,7 @@ from .models import Student
 # Create your views here.
 class StudentListview(ListView):
     model=Student
+    # template_name_suffix='_list'
+    #template_name_suffix if we want to change in ours default and make ours some things own custom
+    #we also do ordering here
+    ordering=['name']
